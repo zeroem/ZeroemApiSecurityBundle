74209d92671d271ceff54da3e7909decabbe27bc
Darrell Hamilton <darrell.noice@gmail.com>
2012-03-13 01:48:13 -0700

    further implementation

--- Security/Authentication/Provider/ApiProvider.php
+++ Security/Authentication/Provider/ApiProvider.php
@@ -5,6 +5,8 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\NonceExpiredException;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\HttpFoundation\Request;
+
 use Zeroem\ApiAuthenticationBundle\Security\Authentication\Token\ApiToken;
 
 class ApiProvider implements AuthenticationProviderInterface
@@ -12,12 +14,72 @@ class ApiProvider implements AuthenticationProviderInterface
     private $userProvider;
     private $cacheDir;
 
+    private static $requiresContentMd5 = array("POST","PUT","PATCH");
+
     public function __construct(UserProviderInterface $userProvider, $cacheDir) {
         $this->userProvider = $userProvider;
         $this->cacheDir     = $cacheDir;
     }
 
     public function authenticate(TokenInterface $token) {
+        $apiUser = $this->userProvider->loadUserByUsername($token->apiToken);
 
+        if($apiUser && $this->validateRequest($token->request, $user, $token->signature)) {
+
+        }
+    }
+
+    protected function validateRequest(Request $request, UserInterface $user, $providedSignature) {
+
+        // Enforce a time limit on the request
+        if(!$this->isValidTimestamp(strtotime($request->headers->get("date")))) {
+            return false;
+        }
+        
+        $parts = array()
+        $parts[] = $request->getMethod();
+        if($request->headers->has("content-md5")) {
+            $parts[] = $request->headers->get("content-md5");
+        } else if(in_array($request->getMethod(),self::$requiresContentMd5)) {
+            return false;
+        }
+
+        $parts[] = $request->headers->get("date");
+        $parts[] = $this->makeResource($request);
+
+        $message = implode("\n",$parts);
+
+        $calculatedSignature = hash_hmac("sha256",$message,$user->getPassword();
+
+        return $calculatedSignature == $providedSignature;
+    }
+
+           protected function isValidTimestamp($timestamp) {
+            return false !== $timestamp && (abs(time() - $timestamp) < 300)
+        }
+
+    protected function makeResource(Request $request) {
+        $resource = $request->getBaseUrl();
+
+        $queryArray = $request->query->all();
+        if(!empty($queryArray)) {
+            ksort($queryArray,SORT_STRING);
+            $params = "";
+            foreach($queryArray as $key=>$value) {
+                if(!empty($params)) {
+                    $params .= "&";
+                }
+
+                $params .= "{$key}={$value}";
+            }
+
+            $resource .= "?{$params}";
+        }
+
+        return $resource;
+    }
+
+    public function supports(TokenInterface $token) {
+        return $token instanceof ApiToken;
     }
 }
