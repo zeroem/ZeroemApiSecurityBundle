92fe59c2872d8711c40afd3ad25a912257aebfcb
Darrell Hamilton <darrell.noice@gmail.com>
2012-03-14 22:30:15 -0700

    the missing bundle class...

---
+++ ZeroemApiSecurityBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * (c) Darrell Hamilton <darrell.noice@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zeroem\ApiSecurityBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ZeroemApiSecurityBundle extends Bundle
+{
+
+}
